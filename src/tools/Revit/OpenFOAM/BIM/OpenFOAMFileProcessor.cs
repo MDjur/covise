69a124676ae56ba4d45942f32528f17bac0014bb
cchyland <cchyland@email.arizona.edu>
2022-07-20 15:32:54 +0200

    Added file to begin processing a config file for program data

---
+++ src/tools/Revit/OpenFOAM/BIM/OpenFOAMFileProcessor.cs
@@ -0,0 +1,25 @@
+﻿/* This file is part of COVISE.
+
+   You can use it under the terms of the GNU Lesser General Public License
+   version 2.1 or later, see lgpl-2.1.txt.
+
+ * License: LGPL 2+ */
+
+using System;
+
+namespace OpenFOAMInterface.BIM
+
+public class OpenFOAMFileProcessor
+{
+	public OpenFOAMFileProcessor(in string filename)
+	{
+		try
+        {
+            string[] lines = File.readAllLines(filename);
+        } catch (Exception e)
+        {
+            OpenFOAMDialogManager.ShowDialogException(e);
+            return;
+        }
+	}
+}
