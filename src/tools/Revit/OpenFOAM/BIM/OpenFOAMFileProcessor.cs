3c917c24ce6613b5cee4904f0d685d24d9809f9b
cchyland <cchyland@email.arizona.edu>
2022-07-20 17:03:06 +0200

    comment handling in the new file processing class

--- src/tools/Revit/OpenFOAM/BIM/OpenFOAMFileProcessor.cs
+++ src/tools/Revit/OpenFOAM/BIM/OpenFOAMFileProcessor.cs
@@ -6,20 +6,71 @@
  * License: LGPL 2+ */
 
 using System;
+using System.IO;
 
 namespace OpenFOAMInterface.BIM
-
-public class OpenFOAMFileProcessor
 {
-	public OpenFOAMFileProcessor(in string filename)
-	{
-		try
+    public class OpenFOAMFileProcessor
+    {
+        string[] lines;
+
+        public OpenFOAMFileProcessor(in string filename)
         {
-            string[] lines = File.readAllLines(filename);
-        } catch (Exception e)
-        {
-            OpenFOAMDialogManager.ShowDialogException(e);
-            return;
+            try //read file
+            {
+                lines = File.readAllLines(filename);
+            }
+            catch (Exception e) //error reading file
+            {
+                OpenFOAMDialogManager.ShowDialogException(e);
+                return;
+            }
+
+            removeComments();
+            extractFileData();
         }
-	}
+
+        private removeComments()
+        {
+            for (int idx = 0; idx < lines.Length; idx++)
+            {
+                //handle single line comments
+                if (lines[idx].Contains("//"))
+                    lines[idx] = lines[idx].Substring(0, lines[idx].IndexOf("//");
+
+                //handle block (multi-line) comments
+                if (lines[idx].Contains("/*"))
+                {
+                    if (lines[idx].Contains("*/")) //remove all block comment text where block comment is in a single line
+                    {
+                        lines[idx] = lines[idx].Substring(0, lines[idx].IndexOf("/*")) + lines[idx].Substring(lines[idx].IndexOf("*/")+2);
+                    } else
+                    {
+                        //remove the start of the block comment but preserve any text in the line before it begins
+                        if (idx + 1 >= lines.Length)
+                            throw new OpenFOAMFileFormatException("Multi-line comment is not closed properly."); //TODO: finish
+                        lines[idx++] = lines[idx].Substring(0, lines[idx].IndexOf("/*")); //move to the next line (as this line has already been treated to remove the comment
+                        while (!lines[idx].Contains("*/"))
+                            lines[idx++] = "";
+                        lines[idx] = lines[idx].Substring(lines[idx].IndexOf("*/") + 2);
+                    }
+                }
+            }
+        }
+
+        private extractFileData()
+        {
+            //TODO: implement
+        }
+
+
+    }
+
+    public class OpenFOAMFileFormatException : Exception
+    {
+        public OpenFOAMFileFormatException(string message)
+        {
+            //TODO: Check Ideal Implementation 
+        }
+    }
 }
