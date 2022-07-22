1ede7064b496e383a58718cb2fa1e09bd10bdd97
cchyland <cchyland@email.arizona.edu>
2022-07-22 10:19:34 +0200

    updated cmake and added to new file for config file processing

--- src/tools/Revit/OpenFOAM/BIM/OpenFOAMFileProcessor.cs
+++ src/tools/Revit/OpenFOAM/BIM/OpenFOAMFileProcessor.cs
@@ -7,18 +7,24 @@
 
 using System;
 using System.IO;
+using System.Collections.Generic;
+using System.Windows.Forms;
 
 namespace OpenFOAMInterface.BIM
 {
     public class OpenFOAMFileProcessor
     {
-        string[] lines;
+        private string filename;
+        private string[] lines;
+        private Dictionary<string, Dictionary<string, string>> fileData;
 
         public OpenFOAMFileProcessor(in string filename)
         {
+            this.filename = filename;
+            this.fileData = new Dictionary<string, Dictionary<string, string>>();
             try //read file
             {
-                lines = File.readAllLines(filename);
+                this.lines = File.ReadAllLines(filename);
             }
             catch (Exception e) //error reading file
             {
@@ -26,41 +32,97 @@ namespace OpenFOAMInterface.BIM
                 return;
             }
 
-            removeComments();
+            isolateFileData();
             extractFileData();
         }
 
-        private removeComments()
+        private void isolateFileData()
         {
             for (int idx = 0; idx < lines.Length; idx++)
             {
-                //handle single line comments
+                //remove single line comments
                 if (lines[idx].Contains("//"))
-                    lines[idx] = lines[idx].Substring(0, lines[idx].IndexOf("//");
+                    lines[idx] = lines[idx].Substring(0, lines[idx].IndexOf("//"));
 
-                //handle block (multi-line) comments
+                //remove block (multi-line) comments
                 if (lines[idx].Contains("/*"))
                 {
                     if (lines[idx].Contains("*/")) //remove all block comment text where block comment is in a single line
                     {
-                        lines[idx] = lines[idx].Substring(0, lines[idx].IndexOf("/*")) + lines[idx].Substring(lines[idx].IndexOf("*/")+2);
+                        lines[idx] = lines[idx].Substring(0, lines[idx].IndexOf("/*")) + lines[idx].Substring(lines[idx].IndexOf("*/") + 2);
                     } else
                     {
                         //remove the start of the block comment but preserve any text in the line before it begins
                         if (idx + 1 >= lines.Length)
-                            throw new OpenFOAMFileFormatException("Multi-line comment is not closed properly."); //TODO: finish
+                            throw new OpenFOAMFileFormatException("Multi-line comment is not closed properly in config file " + filename + ".");
                         lines[idx++] = lines[idx].Substring(0, lines[idx].IndexOf("/*")); //move to the next line (as this line has already been treated to remove the comment
                         while (!lines[idx].Contains("*/"))
                             lines[idx++] = "";
                         lines[idx] = lines[idx].Substring(lines[idx].IndexOf("*/") + 2);
                     }
                 }
+
+                //remove leading and trailing whitespace from every line
+                lines[idx] = lines[idx].Trim();
             }
         }
 
-        private extractFileData()
+        private void extractFileData()
         {
-            //TODO: implement
+            //TODO: handle FoamFile information
+            int lineNum = 0;
+            while (lineNum < lines.Length)
+            {
+                if (String.IsNullOrEmpty(lines[lineNum]))
+                    lineNum++; //skip any blank lines
+                else if (lineNum + 2 >= lines.Length)
+                    throw new OpenFOAMFileFormatException("Improper or incomplete data contained in config file " + filename + " beginning at line " + lineNum + ".");
+                else
+                    lineNum = processDictionaryEntry(lineNum);
+            }
+        }
+
+        private int processDictionaryEntry(int lineNum)
+        {
+            string key = lines[lineNum++]; //identify the key and advance to determine value type
+            while (lineNum < lines.Length && String.IsNullOrEmpty(lines[lineNum]))
+                lineNum++; //skip any blank lines
+            if (lineNum >= lines.Length || !lines[lineNum].Equals("{"))
+                throw new OpenFOAMFileFormatException("Improper dictionary syntax in config file " + filename + ".");
+            else
+            {
+                lineNum++; //skip opening brace
+                Dictionary<string, string> dict = new Dictionary<string, string>();
+                while (lineNum < lines.Length && !lines[lineNum].Equals("}")) {
+                    if (String.IsNullOrEmpty(lines[lineNum]))
+                        continue; //skip any blank lines
+                    string[] currLine = lines[lineNum].Split('\t');
+                    string currKey = "";
+                    string currVal = "";
+                    foreach (string section in currLine)
+                    {
+                        section.Trim(); //remove remaining whitespace on sections
+                        if (String.IsNullOrEmpty(section))
+                            continue;
+                        else if (String.IsNullOrEmpty(currKey))
+                            currKey = section;
+                        else if (String.IsNullOrEmpty(currVal))
+                            currVal = section;
+                        else
+                            throw new OpenFOAMFileFormatException("Improper dictionary entry syntax in config file " + filename + " at line number " + lineNum + ".");
+                    }
+                    if (String.IsNullOrEmpty(currKey) || String.IsNullOrEmpty(currVal))
+                        throw new OpenFOAMFileFormatException("Improper dictionary entry syntax in config file " + filename + " at line number " + lineNum + ".");
+                    dict.Add(currKey, currVal);
+                    lineNum++; //current line has been processed, so advance to the next line and continue
+                }
+                if (lineNum >= lines.Length)
+                    throw new OpenFOAMFileFormatException("Improper dictionary syntax in config file " + filename + ".");
+                else if (lines[lineNum].Equals("}"))
+                    lineNum++; //skip closing brace
+                fileData.Add(key, dict);
+            }
+            return lineNum;
         }
 
 
@@ -70,7 +132,8 @@ namespace OpenFOAMInterface.BIM
     {
         public OpenFOAMFileFormatException(string message)
         {
-            //TODO: Check Ideal Implementation 
+            MessageBox.Show(message, OpenFOAMInterfaceResource.MESSAGE_BOX_TITLE, MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            return;
         }
     }
 }
