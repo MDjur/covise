7d681a39bb09a1b4843f987877745a4fd11d0e5f
cchyland <cchyland@email.arizona.edu>
2022-07-27 11:19:49 +0200

    updates and commenting for file processor (and exception constructors)

--- src/tools/Revit/OpenFOAM/BIM/OpenFOAMFileProcessor.cs
+++ src/tools/Revit/OpenFOAM/BIM/OpenFOAMFileProcessor.cs
@@ -12,13 +12,23 @@ using System.Windows.Forms;
 
 namespace OpenFOAMInterface.BIM
 {
+
+    /// <summary>
+    /// This class is designed to process config files written in the OpenFOAM syntax style and store the filename, file text (currently inaccessible outside of the class),
+    /// file background data, and file contents (the last two of which are acheived through file processing in the constructor) in order to use the information from the file 
+    /// to set base and default values.
+    /// </summary>
     public class OpenFOAMFileProcessor
     {
-        private string filename;
-        private string[] lines;
-        private Dictionary<string, string> fileData;
-        private Dictionary<string, Dictionary<string, string>> fileContents;
+        private string filename; //name of the file being processed by the instance of the class
+        private string[] lines; //contents of the file broken into an array by occurences of newline characters
+        private Dictionary<string, string> fileData; //background information about the file itself
+        private Dictionary<string, Dictionary<string, string>> fileContents; //dictionary information stored in file
 
+        /// <summary>
+        /// Constructor that creates a new file processor instance for the specific file provided (by filename) and parses the file to populate all class fields
+        /// </summary>
+        /// <param name="filename">String containing the name of the file to be processed</param>
         public OpenFOAMFileProcessor(in string filename)
         {
             this.filename = filename;
@@ -38,6 +48,9 @@ namespace OpenFOAMInterface.BIM
             extractFileData();
         }
 
+        /// <summary>
+        /// This is a private helper method for the constructor which performs preprocessing on the lines in the file to remove comments and leading and trailing whitespace
+        /// </summary>
         private void isolateFileData()
         {
             for (int idx = 0; idx < lines.Length; idx++)
@@ -68,6 +81,10 @@ namespace OpenFOAMInterface.BIM
             }
         }
 
+        /// <summary>
+        /// This is a private helper method for the constructor which parses data from the input file (after it has undergone preprocessing in isolateFileData() 
+        /// in order to populate both the fileData and fileContents fields.
+        /// </summary>
         private void extractFileData()
         {
             int lineNum = 0;
@@ -84,6 +101,12 @@ namespace OpenFOAMInterface.BIM
             }
         }
 
+        /// <summary>
+        /// This is a private helper method for the extractFileData method, which takes in line number expected to contain information about the file and processes it accordingly.
+        /// </summary>
+        /// <param name="lineNum">int indicating the current line that must be processed (in connection with any later lines related to it)</param>
+        /// <returns>int indicating the new line number after processing all relevant information for the original line number</returns>
+        /// <throws>OpenFOAMFileFormatException indicating that there is a syntax error in the file being processed if the information is not presented as expected</throws>
         private int processFileData(int lineNum)
         {
             lineNum++; //skip file data header
@@ -135,6 +158,12 @@ namespace OpenFOAMInterface.BIM
             return lineNum;
         }
 
+        /// <summary>
+        /// This is a private helper method for the extractFileData method, which takes in line number expected to contain file contents and proceses it accordingly.
+        /// </summary>
+        /// <param name="lineNum">int indicating the current line that must be processed (in connection with any later lines related to it)</param>
+        /// <returns>int indicating the new line number after processing all relevant information for the original line number</returns>
+        /// <throws>OpenFOAMFileFormatException indicating that there is a syntax error in the file being processed if the information is not presented as expected</throws>
         private int processDictionaryEntry(int lineNum)
         {
             string key = lines[lineNum++]; 
@@ -187,58 +216,127 @@ namespace OpenFOAMInterface.BIM
             return lineNum;
         }
 
+        /// <summary>
+        /// This is a public getter method for the version of the file (contained within fileData).
+        /// </summary>
+        /// <returns>string indicating the file version</returns>
+        /// <throws>OpenFOAMFileFormatException indicating that version was not intialized properly if the version data is missing from fileData</throws>
         public string getFileVersion()
         {
             if (fileData.ContainsKey("version"))
                 return fileData["version"];
             else
-                throw new OpenFOAMFileFormatException("Background information not initialized properly for config file " + filename + ".");
+                throw new OpenFOAMFileFormatException("Version information not initialized properly in config file " + filename + ".");
         }
 
+        /// <summary>
+        /// This is a public getter method for the name of the file (contained within private field filename).
+        /// </summary>
+        /// <returns>string indicating the file name</returns>
+        public string getFilename() => filename;
+
+        /// <summary>
+        /// This is a public getter method for the format of the file (contained within private field fileData).
+        /// </summary>
+        /// <returns>string indicating the file format</returns>
+        /// <throws>OpenFOAMFileFormatException indicating that format was not intialized properly if the format data is missing from fileData</throws>
         public string getFileFormat()
         {
             if (fileData.ContainsKey("format"))
                 return fileData["format"];
             else
-                throw new OpenFOAMFileFormatException("Background information not initialized properly for config file " + filename + ".");
+                throw new OpenFOAMFileFormatException("Format information not initialized properly in config file " + filename + ".");
         }
 
+        /// <summary>
+        /// This is a public getter method for the class of the file (contained within private field fileData).
+        /// </summary>
+        /// <returns>string indicating the file class</returns>
+        /// <throws>OpenFOAMFileFormatException indicating that class was not intialized properly if the class data is missing from fileData</throws>
         public string getFileClass()
         {
             if (fileData.ContainsKey("class"))
                 return fileData["class"];
             else
-                throw new OpenFOAMFileFormatException("Background information not initialized properly for config file " + filename + ".");
+                throw new OpenFOAMFileFormatException("Class information not initialized properly in config file " + filename + ".");
         }
 
+        /// <summary>
+        /// This is a public getter method for the location of the file (contained within private field fileData).
+        /// </summary>
+        /// <returns>string indicating the file location</returns>
+        /// <throws>OpenFOAMFileFormatException indicating that location was not intialized properly if the location data is missing from fileData</throws>
         public string getFileLocation()
         {
             if (fileData.ContainsKey("location"))
                 return fileData["location"];
             else
-                throw new OpenFOAMFileFormatException("Background information not initialized properly for config file " + filename + ".");
+                throw new OpenFOAMFileFormatException("Location information not initialized properly in config file " + filename + ".");
         }
 
+        /// <summary>
+        /// This is a public getter method for the object of the file (contained within private field fileData).
+        /// </summary>
+        /// <returns>string indicating the file object</returns>
+        /// <throws>OpenFOAMFileFormatException indicating that object was not intialized properly if the object data is missing from fileData</throws>
         public string getFileObject()
         {
             if (fileData.ContainsKey("object"))
                 return fileData["object"];
             else
-                throw new OpenFOAMFileFormatException("Background information not initialized properly for config file " + filename + ".");
+                throw new OpenFOAMFileFormatException("Object information not initialized properly in config file " + filename + ".");
         }
 
-        public Dictionary<string, Dictionary<string, string>> getFileContents()
-        {
-            return fileContents;
-        }
+        /// <summary>
+        /// This is a public getter method for the contents of the file (or the entirety of the private field fileContents).
+        /// </summary>
+        /// <returns>Dictionary<string, Dictionary<string, string>> containing the contents of the parsed file</returns>
+        public Dictionary<string, Dictionary<string, string>> getFileContents() => fileContents;
     }
 
+    /// <summary>
+    /// This is an exception class written to represent file format exceptions in an OpenFOAM config file processed by this class.  It contains four constructors 
+    /// for these types of errors: one which is a basic default constructor, one which sets only the message, one which sets the message and inner exception properties, and 
+    /// one which makes it serializable.
+    /// </summary>
+    [Serializable]
     public class OpenFOAMFileFormatException : Exception
     {
-        public OpenFOAMFileFormatException(string message)
+        /// <summary>
+        /// This is the default constructor for the OpenFOAMFileFormatException, which simply relies on the base Excpetion constructor to supply its message
+        /// </summary>
+        public OpenFOAMFileFormatException() : base() { }
+
+        /// <summary>
+        /// This is the constructor for an OpenFOAMFileFormatException with a specific error message (provided via parameter) containing information regarding the type of syntax
+        /// error which triggered the excpetion in the file (potentially including a filename and/or line number).
+        /// </summary>
+        /// <param name="message">This is the message providing information about the syntax error that occurred</param>
+        public OpenFOAMFileFormatException(string message) : base(message) 
+        {
+            //currently all instances of the exception utilize this constructor
+            MessageBox.Show(message, OpenFOAMInterfaceResource.MESSAGE_BOX_TITLE, MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            return;
+        }
+
+        /// <summary>
+        /// This is the constructor for an OpenFOAMFileFormatException with a specific error message (provided via parameter) containing information regarding the type of syntax
+        /// error which triggered the excpetion in the file (potentially including a filename and/or line number) along with innerException information to pass to the base 
+        /// Exception constructor.
+        /// </summary>
+        /// <param name="message">This is the message providing information about the syntax error that occurred</param>
+        /// <param name="innerException">This is the innerException infomration regarding the current exception and how it occurred</param>
+        public OpenFOAMFileFormatException(string message, Exception innerException) : base(message, innerException)
         {
             MessageBox.Show(message, OpenFOAMInterfaceResource.MESSAGE_BOX_TITLE, MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
             return;
         }
+
+        /// <summary>
+        /// This is the constructor for an OpenFOAMFileFormatException which makes it serializable.
+        /// </summary>
+        /// <param name="info">System.Runtime.Serialization.SerializationInfo necessary for serialization</param>
+        /// <param name="context">System.Runtime.Serialization.StreamingContext necessary for serialization</param>
+        protected OpenFOAMFileFormatException(System.Runtime.Serialization.SerializationInfo info, System.Runtime.Serialization.StreamingContext context) : base(info, context) { }
     }
 }
