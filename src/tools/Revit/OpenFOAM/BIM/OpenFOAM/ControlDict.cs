c707080a975afbc08f6ea19173e5e8603a658b02
Marko Djuric <marko.djuric@gmx.de>
2022-09-13 15:41:11 +0200

    OpenFOAMInterface: remove unnecessary defaults

--- src/tools/Revit/OpenFOAM/BIM/OpenFOAM/ControlDict.cs
+++ src/tools/Revit/OpenFOAM/BIM/OpenFOAM/ControlDict.cs
@@ -40,9 +40,7 @@ namespace OpenFOAMInterface.BIM.OpenFOAM
         /// </summary>
         public override void InitAttributes()
         {
-            FoamFile.Attributes.Add("application", FOAMInterface.Singleton.Data.ControlDictParameters.AppControlDictSolver);
             base.InitAttributes();
-            FoamFile.Attributes.Add("functions", m_Functions);
         }
 
         /// <summary>
