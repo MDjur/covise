1ffac29d52310fb9190bbe9b89f05cd9dc7de403
MDjur <marko.djuric@gmx.de>
2025-02-19 15:53:08 +0100

    EnergyCampus: fix color computing for heating

--- src/OpenCOVER/plugins/hlrs/Energy/lib/core/simulation/heating.h
+++ src/OpenCOVER/plugins/hlrs/Energy/lib/core/simulation/heating.h
@@ -4,7 +4,6 @@
 #include <string>
 #include <vector>
 
-
 namespace core::simulation::heating {
 
 typedef std::map<std::string, std::vector<double>> Data;
@@ -33,8 +32,8 @@ class Base {
   Data m_data;  // timestep data
 };
 
-// Producer and Consumer are derived from Base => probably will differ more in the future
-// if heating sim is final
+// Producer and Consumer are derived from Base => probably will differ more in the
+// future if heating sim is final
 class Producer : public Base {
  public:
   Producer(const std::string &name, const Data &data = {}) : Base(name, data) {}
@@ -88,25 +87,48 @@ class HeatingSimulation {
   auto &getData() { return m_data; }
   const auto &getConsumers() const { return m_consumers; }
   const auto &getProducers() const { return m_producers; }
+  const auto &getMinMax(const std::string &key) const { return m_minMax.at(key); }
+  const auto &getTimesteps(const std::string &key) const {
+    return m_timesteps.at(key);
+  }
+
+  void computeParameters();
 
  private:
-  template <typename U>
-  void addBase(std::map<std::string, U> &baseMap, const std::string &name,
+  template <typename T>
+  void addBase(std::map<std::string, T> &baseMap, const std::string &name,
                const Data &data = {}) {
-    static_assert(std::is_base_of_v<Base, U>, "U must be derived from Base");
+    static_assert(std::is_base_of_v<Base, T>, "T must be derived from Base");
     baseMap.insert({name, {name, data}});
   }
 
-  template <typename U, typename... Args>
-  void addDataToBase(std::map<std::string, U> &baseMap, const std::string &name,
+  template <typename T, typename... Args>
+  void addDataToBase(std::map<std::string, T> &baseMap, const std::string &name,
                      Args &&...args) {
-    static_assert(std::is_base_of_v<Base, U>, "U must be derived from Base");
+    static_assert(std::is_base_of_v<Base, T>, "T must be derived from Base");
     if (auto it = baseMap.find(name); it != baseMap.end())
       it->second.addData(std::forward<Args>(args)...);
   }
 
+  template <typename T>
+  void computeParameter(const std::map<std::string, T> &baseMap) {
+    static_assert(std::is_base_of_v<Base, T>, "T must be derived from Base");
+    for (const auto &[_, base] : baseMap) {
+      const auto &data = base.getData();
+      for (const auto &[key, values] : data) {
+        computeMinMax(key, values);
+        computeMaxTimestep(key, values);
+      }
+    }
+  }
+
+  void computeMinMax(const std::string &key, const std::vector<double> &values);
+  void computeMaxTimestep(const std::string &key, const std::vector<double> &values);
+
   std::map<std::string, Producer> m_producers;
   std::map<std::string, Consumer> m_consumers;
+  std::map<std::string, std::pair<double, double>> m_minMax;
+  std::map<std::string, size_t> m_timesteps;
   Data m_data;
 };
 }  // namespace core::simulation::heating
